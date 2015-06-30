d82b6fac15e083c2010197145224a21cc99cff5a
recasens <recasens@mit.edu>
2015-06-30 10:59:43 -0700

    Accuracy problem solved
    Former-commit-id: eba69bbd7dce2f7d67816f9dade193b14dc5f9bb [formerly fea302b1354c6621fd98f7878518423a975b3ec2] [formerly a41f5db8245263dd24153edaef3c4f4b4fecd8fa [formerly a41f5db8245263dd24153edaef3c4f4b4fecd8fa [formerly 07d230f49987315de7e939a320f78035f94252a3]]]
    Former-commit-id: 4732a936241f7f916af8b8b236f4528537d66379 [formerly db9b2711efd4b67b55f83fda37f475c97f9dfd11]
    Former-commit-id: 06afb9603217e3e63e498534880db586f84124ab
    Former-commit-id: 421f0d0cdee9fd7646b9c1d7ffd8b471a763d561

--- trainer.py
+++ trainer.py
@@ -153,7 +153,7 @@ def train(experiment_file, out_path=None, **kwargs):
                     inps = data['train'].next()
                 except StopIteration:
                     btime = time.time()
-                    monitor.disp(e, data['train'].count,btime-atime)
+                    monitor.disp(e, 1,btime-atime)
                     break
 
                 outs = f_grad_shared(*inps)
@@ -165,7 +165,7 @@ def train(experiment_file, out_path=None, **kwargs):
                 btime = time.time()
                 if display_interval is not None and s == display_interval:
                     s = 0
-                    monitor.disp(e, data['train'].count, btime - atime)
+                    monitor.disp(e, 1, btime - atime)
                     if out_path is not None:
                         save_images = data['train'].dataset.save_images
                         rnn_samples = train_o['cond_gen_gru_x'][:, :10]
