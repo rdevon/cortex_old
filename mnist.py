76840705a8b3eb36c2e3ee626cc2bbc236ba5986
rdevon <devon@cs.unm.edu>
2015-07-14 15:22:21 -0700

    Added RNN Added chain model

--- mnist.py
+++ mnist.py
@@ -1,7 +1,15 @@
 import cPickle
 import gzip
+import igraph
+from igraph import Graph
+import multiprocessing as mp
 import numpy as np
 import PIL
+import random
+import sys
+import theano
+from theano import tensor as T
+import traceback
 from vis_utils import tile_raster_images
 
 def get_iter(inf=False, batch_size=128):
@@ -10,7 +18,7 @@ def get_iter(inf=False, batch_size=128):
 class mnist_iterator:
     def __init__(self, batch_size=128, data_file='/Users/devon/Data/mnist.pkl.gz',
                  restrict_digits=None, mode='train', shuffle=True, inf=False,
-                 repeat=1):
+                 out_mode='', repeat=1, chain_length=20):
         # load MNIST
         with gzip.open(data_file, 'rb') as f:
             x = cPickle.load(f)
@@ -21,11 +29,37 @@ class mnist_iterator:
         elif mode == 'valid':
             X = np.float32(x[1][0])
             Y = np.float32(x[1][1])
-        elif model == 'test':
+        elif mode == 'test':
             X = np.float32(x[2][0])
             Y = np.float32(x[2][1])
         else:
             raise ValueError()
+
+        self.f_energy = None
+        self.pairs = None
+        self.chain = None
+        self.chain_idx = None
+        self.chain_length = chain_length
+        self.chains = None
+        self.mode = out_mode
+
+        if self.mode == 'pairs':
+            self.next = self._next_pairs
+            self.pairs = np.load('pairs.npy').tolist()
+        elif self.mode == 'chains':
+            self.next = self._next_chains
+            self.chain = np.load('chain.npy').tolist()
+            self.chain_idx = range(0, len(self.chain) - chain_length)
+            random.shuffle(self.chain_idx)
+        elif self.mode == 'multi_chains':
+            self.next = self._next_multi_chains
+            self.chains = np.load('chains.npy').tolist()
+            self.chain_length = len(self.chains[0])
+            random.shuffle(self.chains)
+        elif self.mode == 'model_chains':
+            self.next = self._next_model_chains
+        else:
+            self.next = self._next
 
         if restrict_digits is None:
             n_classes = 10
@@ -62,18 +96,137 @@ class mnist_iterator:
         self.O = O
 
         # randomize
-        if self.shuffle:
+        if self.shuffle and not self.mode in ['pairs', 'chains', 'multi_chains']:
+            print 'Shuffling mnist'
             self.randomize()
 
     def __iter__(self):
         return self
 
+    def set_f_energy(self, f_energy):
+        self.f_energy = f_energy
+
     def randomize(self):
-        rnd_idx = np.random.permutation(np.arange(0, self.X.shape[0], 1));
+        rnd_idx = np.random.permutation(np.arange(0, self.X.shape[0], 1))
         self.X = self.X[rnd_idx, :];
         self.O = self.O[rnd_idx, :];
 
     def next(self):
+        raise NotImplementedError()
+
+    def _next_pairs(self):
+        assert self.pairs is not None
+        cpos = self.pos
+        if cpos == -1:
+            # reset
+            self.pos = 0
+            cpos = self.pos
+            if self.shuffle:
+                random.shuffle(self.pairs)
+            if not self.inf:
+                raise StopIteration
+
+        pairs = [self.pairs[i] for i in xrange(cpos, cpos + self.bs)]
+
+        if cpos + 2 * self.bs >= len(self.pairs):
+            self.pos = -1
+        else:
+            self.pos += self.bs
+
+        x = np.zeros((2, self.bs, self.dim)).astype('float32')
+        for i, p in enumerate(pairs):
+            x[0, i] = self.X[p[0]]
+            x[1, i] = self.X[p[1]]
+
+        return x, None
+
+    def _next_chains(self):
+        assert self.chain is not None
+        cpos = self.pos
+        if cpos == -1:
+            # reset
+            self.pos = 0
+            cpos = self.pos
+            if self.shuffle:
+                random.shuffle(self.chain_idx)
+            if not self.inf:
+                raise StopIteration
+
+        chains = [[self.chain[i]
+                   for i in xrange(self.chain_idx[j],
+                                   self.chain_idx[j] + self.chain_length)]
+            for j in xrange(cpos, cpos + self.bs)]
+
+        if cpos + 2 * self.bs >= len(self.chain_idx):
+            self.pos = -1
+        else:
+            self.pos += self.bs
+
+        x = np.zeros((self.chain_length, self.bs, self.dim)).astype('float32')
+        for i, c in enumerate(chains):
+            x[:, i] = self.X[c]
+
+        return x, None
+
+    def _next_multi_chains(self):
+        assert self.chains is not None
+        cpos = self.pos
+        if cpos == -1:
+            # reset
+            self.pos = 0
+            cpos = self.pos
+            if self.shuffle:
+                for chain in self.chains:
+                    random.shuffle(chain)
+                random.shuffle(self.chains)
+            if not self.inf:
+                raise StopIteration
+
+        chains = [self.chains[i] for i in xrange(cpos, cpos + self.bs)]
+
+        if cpos + 2 * self.bs >= len(self.chains):
+            self.pos = -1
+        else:
+            self.pos += self.bs
+
+        x = np.zeros((self.chain_length, self.bs, self.dim)).astype('float32')
+        for i, c in enumerate(chains):
+            x[:, i] = self.X[c]
+
+        return x, None
+
+    def _next_model_chains(self):
+        assert self.f_energy is not None
+
+        data = np.zeros((self.chain_length, self.bs, self.dim)).astype('float32')
+        last_batch, _ = self._next()
+        data[0] = last_batch
+
+        energies = np.zeros((self.bs,))
+        for y in xrange(1, self.chain_length):
+            last_batch = data[y - 1]
+            batch, _ = self._next()
+            batch_idx = range(last_batch.shape[0])
+            random.shuffle(batch_idx)
+
+            for x in batch:
+                min_e = float('inf')
+                for k, j in enumerate(batch_idx):
+                    energy = self.f_energy(last_batch[j], x)
+                    if energy < min_e:
+                        min_e = energy
+                        pop_idx = k
+                        min_idx = j
+                energies[min_idx] += min_e
+                data[y, min_idx] = x
+                batch_idx.pop(pop_idx)
+
+        idx = np.argsort(energies).tolist()
+        data = data[:, idx]
+
+        return data, None
+
+    def _next(self):
         cpos = self.pos
         if cpos == -1:
             # reset
@@ -86,16 +239,23 @@ class mnist_iterator:
 
         x = self.X[cpos:cpos+self.bs]
         y = self.O[cpos:cpos+self.bs]
-        x = np.concatenate([x for _ in xrange(self.repeat)], axis=0)
-        y = np.concatenate([y for _ in xrange(self.repeat)], axis=0)
-        cbatch = (x, y)
+        x = np.concatenate([x] * self.repeat, axis=0)
+        y = np.concatenate([y] * self.repeat, axis=0)
 
         if cpos + 2 * self.bs >= self.n:
             self.pos = -1
         else:
             self.pos += self.bs
 
-        return cbatch
+        if self.mode == 'symmetrize_and_pair':
+            new_x = np.zeros((2, self.batch_size * self.batch_size, x.shape[1])).astype('float32')
+            for i in xrange(batch_size):
+                for j in xrange(batch_size):
+                    batch = np.concatenate([x[i][None, :], x[j][None, :]])
+                    new_x[:, self.batch_size * j + i] = batch
+            x = new_x
+
+        return x, y
 
     def save_images(self, x, imgfile, transpose=False):
         tshape = x.shape[0], x.shape[1]
@@ -114,3 +274,88 @@ class mnist_iterator:
         return PIL.Image.fromarray(tile_raster_images(
             X=X, img_shape=fshape, tile_shape=tshape,
             tile_spacing=(1, 1)))
+
+    def translate(self, x):
+        return x
+
+def print_graph(mat, ys, thr=0.1, graph_file=None, mode='multilevel'):
+    colors = {
+        0: 'blue',
+        1: 'red',
+        2: 'orange',
+        3: 'green',
+        4: 'purple',
+        5: 'brown',
+        6: 'white',
+        7: 'yellow',
+        8: 'cyan',
+        9: 'tan',
+        10: 'magenta',
+        11: 'grey',
+        12: 'black'}
+
+    max_weight = np.max(mat)
+    thr = thr * max_weight
+    wheres = np.where(mat > thr)
+
+    edgelist = []
+    weights = []
+    for x, y in zip(wheres[0], wheres[1]):
+        if x < y:
+            edgelist.append([x, y])
+            weights.append(mat[x, y])
+    weights = weights / np.std(weights)
+    graph = Graph(edgelist, directed=False)
+    graph.vs['label'] = ys
+
+    if mode == 'eigenvector':
+        cl = graph.community_leading_eigenvector(clusters=10)
+    elif mode == 'multilevel':
+        cls = graph.community_multilevel(return_levels=True, weights=weights)
+        cl = list(cls[0])
+
+    print cl
+    print [[ys[c] for c in cls] for cls in cl]
+    for c, g in enumerate(cl):
+        graph.vs(cl[c])['color'] = colors[c % 13]
+
+    igraph.plot(graph, graph_file, weights=weights, edge_width=weights/10, vertex_label_size=8)
+
+counter = None
+
+def init(_counter):
+    global counter
+    counter = _counter
+
+def make_chain((chain, X)):
+    global counter
+    idx = range(50000)
+    random.shuffle(idx)
+    for k in idx:
+        if k != chain[-1] and np.corrcoef(X[chain[-1]], X[k])[0, 1] >= 0.8:
+            chain.append(k)
+        if len(chain) == 10:
+            break
+    if len(chain) == 10:
+        counter.value += 1
+        sys.stdout.write('\r%d' % counter.value); sys.stdout.flush()
+    else:
+        chain = []
+    return chain
+
+def make_chains():
+    m = mnist_iterator(out_mode='chains', batch_size=1)
+    rnd_idx = np.random.permutation(np.arange(0, m.X.shape[0], 1))
+
+    counter = mp.Value('i', 0)
+    p = mp.Pool(100, initializer=init, initargs=(counter, ))
+    try:
+        i = p.map_async(make_chain, [([i], m.X) for i in rnd_idx])
+        i.wait()
+        chains = i.get()
+    except Exception as e:
+        traceback.print_exc(file=sys.stdout)
+    finally:
+        p.terminate()
+        p.join()
+    return chains
--- rbm.py
+++ rbm.py
@@ -18,11 +18,16 @@ norm_weight = tools.norm_weight
 ortho_weight = tools.ortho_weight
 
 class RBM(Layer):
-    def __init__(self, dim_in, dim_h, name='rbm', trng=None, stochastic=True,
-                 param_file=None, learn=True):
+    def __init__(self, dim_in, dim_h, name='rbm', rng=None, trng=None,
+                 stochastic=True, param_file=None, learn=True):
         self.stochastic = stochastic
         self.dim_in = dim_in
         self.dim_h = dim_h
+
+        if rng is None:
+            rng = tools.rng_
+        self.rng = rng
+
         if trng is None:
             self.trng = RandomStreams(6 * 10 * 2015)
         else:
@@ -38,7 +43,7 @@ class RBM(Layer):
 
     def set_params(self, param_file=None):
         if param_file is None:
-            W = norm_weight(self.dim_in, self.dim_h)
+            W = norm_weight(self.dim_in, self.dim_h, rng=self.rng)
             b = np.zeros((self.dim_in,)).astype(floatX)
             c = np.zeros((self.dim_h,)).astype(floatX)
         else:
@@ -132,3 +137,124 @@ class RBM(Layer):
         )
 
         return OrderedDict(x=x, h=h, p=p, q=q), updates
+
+
+class GradInferRBM(RBM):
+    def __init__(self, dim_in, dim_h, name='grad_infer_rbm', h_init_mode=None,
+                 trng=None, stochastic=True, param_file=None, learn=True):
+        self.h_init_mode = h_init_mode
+
+        super(GradInferRBM, self).__init__(dim_in, dim_h, name=name, learn=learn,
+                                           stochastic=stochastic, trng=trng)
+
+    def set_params():
+        super(GradInferRBM, self).set_params()
+
+        if self.h_init_mode == 'average':
+            h0 = np.zeros((self.dim_h, )).astype(floatX)
+            self.params.update(h0=h0)
+        elif self.h_init_mode == 'ffn':
+            W0 = norm_weight(self.dim_in, self.dim_h, scale=self.weight_scale,
+                             rng=self.rng)
+            U0 = norm_weight(self.dim_in, self.dim_h, scale=self.weight_scale,
+                             rng=self.rng)
+            b0 = np.zeros((self.dim_h,)).astype('float32')
+            self.params.update(W0=W0, U0=U0, b0=b0)
+        elif self.h_init_mode is not None:
+            raise ValueError(self.h_init_mode)
+
+    def step_h(self, x, h_, XHa, Ura, bha, XHb, Urb, bhb, HX, bx):
+        preact = T.dot(h_, Ura) + T.dot(x, XHa) + bha
+        r, u = self.get_gates(preact)
+        preactx = T.dot(h_, Urb) * r + T.dot(x, XHb) + bhb
+        h = T.tanh(preactx)
+        h = u * h_ + (1. - u) * h
+        return h
+
+    def move_h(self, h0, x, l, HX, bx, *params):
+        h1 = self.step_h(x[0], h0, *params)
+        h = T.concatenate([h0[None, :, :], h1[None, :, :]], axis=0)
+        p = T.nnet.sigmoid(T.dot(h, HX) + bx)
+        energy = self.energy(x, p).mean()
+        grad = theano.grad(energy, wrt=h0, consider_constant=[x])
+        h0 = h0 - l * grad
+        return h0
+
+    def step_infer(self, h0, m, x, l, HX, bx, *params):
+        h0 = self.move_h(h0, x, l, HX, bx, *params)
+        h1 = self.step_h(x[0], h0, *params)
+        h = T.concatenate([h0[None, :, :], h1[None, :, :]], axis=0)
+        p = T.nnet.sigmoid(T.dot(h, HX) + bx)
+        x_hat = self.trng.binomial(p=p, size=p.shape, n=1, dtype=p.dtype)
+        energy = self.energy(x, p)
+
+        return h0, x_hat, p, energy
+
+    def inference(self, x, mask, l, n_inference_steps=1, max_k=10):
+        x0 = x[0]
+        x1 = x[1]
+
+        if self.h0_mode == 'average':
+            h0 = T.alloc(0., x0.shape[0], self.dim_h).astype(floatX) + self.h0[None, :]
+        elif self.h0_mode == 'ffn':
+            h0 = T.dot(x0, self.W0) + T.dot(x0, self.U0) + self.b0
+
+        p0 = T.nnet.sigmoid(T.dot(h0, self.HX) + self.bx)
+
+        seqs = []
+        outputs_info = [x0, p0, h0]
+        non_seqs = self.get_non_seqs()
+
+        (xs, ps, hs), updates = theano.scan(
+            self.step_sample,
+            sequences=seqs,
+            outputs_info=outputs_info,
+            non_sequences=non_seqs,
+            name=tools._p(self.name, 'sample_init'),
+            n_steps=max_k,
+            profile=tools.profile,
+            strict=True
+        )
+
+        xs = T.concatenate([x0[None, :, :], xs], axis=0)
+        x0 = xs[self.k]
+        x_n = T.concatenate([x0[None, :, :], x1[None, :, :]], axis=0)
+
+        if self.h0_mode == 'average':
+            h0 = T.alloc(0., x0.shape[0], self.dim_h).astype(floatX) + self.h0[None, :]
+        elif self.h0_mode == 'ffn':
+            h0 = T.dot(x0, self.W0) + T.dot(x1, self.U0) + self.b0
+
+        h1 = self.step_h(x[0], h0, *self.get_non_seqs())
+        h = T.concatenate([h0[None, :, :], h1[None, :, :]], axis=0)
+        p = T.nnet.sigmoid(T.dot(h, self.HX) + self.bx)
+        x_hat = self.trng.binomial(p=p, size=p.shape, n=1, dtype=p.dtype)
+
+        seqs = []
+        outputs_info = [h0, None, None, None]
+        non_seqs = [mask, x_n, l, self.HX, self.bx] + self.get_non_seqs()
+
+        (h0s, x_hats, ps, energies), updates_2 = theano.scan(
+            self.step_infer,
+            sequences=seqs,
+            outputs_info=outputs_info,
+            non_sequences=non_seqs,
+            name=tools._p(self.name, 'infer'),
+            n_steps=n_inference_steps,
+            profile=tools.profile,
+            strict=True
+        )
+        updates.update(updates_2)
+
+        h0s = T.concatenate([h0[None, :, :], h0s], axis=0)
+        x_hats = T.concatenate([x[None, :, :, :],
+                                x_hat[None, :, :, :],
+                                x_hats], axis=0)
+        energy = self.energy(x, ps[-1])
+
+        if self.h0_mode == 'average':
+            h0_mean = h0s[-1].mean(axis=0)
+            new_h = (1. - self.rate) * h0_mean + self.rate * h0_mean
+            updates += [(self.h0, new_h)]
+
+        return (x_hats, h0s, energy), updates
