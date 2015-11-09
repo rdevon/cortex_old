d42cbb7b49fc92d747c8cfe73a35c407b0860bb0
rdevon <devon@cs.unm.edu>
2015-11-08 17:43:15 -0700

    forgot the mean

--- gbn.py
+++ gbn.py
@@ -124,7 +124,7 @@ class GaussianBeliefNet(Layer):
         log_p_max = T.max(log_p, axis=0, keepdims=True)
         w = T.exp(log_p - log_p_max)
 
-        return T.log(w.sum(axis=0, keepdims=True)) + log_p_max
+        return (T.log(w.sum(axis=0, keepdims=True)) + log_p_max).mean()
 
     def m_step(self, ph, y, q, n_samples=10):
         constants = []
