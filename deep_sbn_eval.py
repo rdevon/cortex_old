92f7ef69c98db05cbbbffcea92852a98579192ed
rdevon <devon@cs.unm.edu>
2015-11-17 11:47:44 -0700

    updated deep sbn code
    Former-commit-id: 02f8333fc0d3fb404d4cb63695c0fbd2edd5f00e [formerly 8f31e5761066b22dd8191118f3472dbf90b367f3] [formerly 4c3b0edc91402876cf001fce466e3c6835e66c1b [formerly 4c3a07ff2127094acd2b384c267f199af235c3c4]]
    Former-commit-id: 84f5ccdf9baebb851f53512b2ae73c95b15ca02d [formerly 65671be69c47b416c59c53ee34ce6953f2ef2bc4]
    Former-commit-id: a443e0ed662c564eb20d67603af3c5d44a289f1f
    Former-commit-id: 3028bd1829f0ccd9b1f4dd75c73c4e6665ada258

--- deep_sbn_eval.py
+++ deep_sbn_eval.py
@@ -25,12 +25,11 @@ from tools import itemlist, load_experiment, load_model
 floatX = theano.config.floatX
 
 def eval_model(
-    model_file, rs=None, n_samples=10000,
+    model_file, steps=50, n_samples=10000,
     out_path=None,
     optimizer=None,
     optimizer_args=dict(),
     batch_size=100,
-    inference_stride=5,
     valid_scores=None,
     mode='valid',
     prior='logistic',
@@ -39,9 +38,9 @@ def eval_model(
     z_init='recognition_net',
     inference_method='momentum',
     inference_rate=.01,
-    n_inference_samples=20,
     n_mcmc_samples=20,
     posterior_samples=20,
+    n_inference_samples=20,
     dataset=None,
     dataset_args=None,
     extra_inference_args=dict(),
@@ -84,65 +83,17 @@ def eval_model(
     else:
         X_i = X
 
-    # ========================================================================
-    print 'Getting initial lower bound'
-
     x, _ = data_iter.next()
     x_v, _ = valid_iter.next()
 
     xs = [x[i: (i + 100)] for i in range(0, n_samples, 100)]
-
     N = len(range(0, n_samples, 100))
-    lbs = []
-    nlls = []
-
-    # ========================================================================
-    print 'Calculating lower bound curve (on 500 samples)'
-
-    if rs is None:
-        rs = range(inference_stride, inference_stride * 10 + 1, inference_stride)
-
-    best_r = 0
-    best_lb = float('inf')
-    lbs = []
-    nlls = []
-    try:
-        for r in rs:
-            print 'number of inference steps: %d' % r
-            outs_s, updates_s = model(X_i, X, n_inference_steps=r,
-                                      n_samples=posterior_samples,
-                                      calculate_log_marginal=True)
-            f_lower_bound = theano.function([X], [outs_s['lower_bound'],
-                                                  outs_s['nll']],
-                updates=updates_s)
-
-            lb, nll = f_lower_bound(x[:500])
-
-            lb_v, nll_v = f_lower_bound(x_v)
-
-            if lb_v < best_lb:
-                best_lb = lb_v
-                best_r = r
-
-            lbs.append(lb)
-            nlls.append(nll)
-            print 'lower bound: %.2f, nll: %.2f' % (lb, nll)
-    except MemoryError:
-        print 'Memory Error. Stopped early.'
-
-    fig = plt.figure()
-    plt.plot(range(len(lbs)), lbs)
-    plt.plot(range(len(lbs)), nlls)
 
     print ('Calculating final lower bound and marginal with %d data samples, %d posterior samples '
-           'with %d validated inference steps' % (N * 100, posterior_samples, rs[-1]))
+           'with %d validated inference steps' % (N * 100, posterior_samples, steps))
 
-    outs_s, updates_s = model(X_i, X, n_inference_steps=rs[-1], n_samples=posterior_samples, calculate_log_marginal=True)
+    outs_s, updates_s = model(X_i, X, n_inference_steps=steps, n_samples=posterior_samples, calculate_log_marginal=True)
     f_lower_bound = theano.function([X], [outs_s['lower_bound'], outs_s['nll']], updates=updates_s)
-
-    #xs = [x[i: (i + 100)] for i in range(0, n_samples, 100)]
-
-    #N = len(range(0, n_samples, 100))
     lb_t = 0.
     nll_t = 0.
 
@@ -170,42 +121,6 @@ def eval_model(
             path.join(out_path, 'samples_from_prior.png'),
             x_limit=10)
 
-    print 'Approximating time to best valid'
-
-    best_valid_epoch = np.argmin(valid_scores)
-
-    print 'Best valid epoch: %d' % best_valid_epoch
-
-    if best_valid_epoch == valid_scores.shape[0]:
-        print 'Didn\'t converge it seems'
-
-    (z, prior_energy, h_energy, y_energy, _), updates, constants = model.inference(
-        X_i, X, n_inference_steps=n_inference_steps, n_samples=n_mcmc_samples)
-
-    cost = prior_energy + h_energy + y_energy
-
-    grads = T.grad(cost, wrt=itemlist(tparams),
-                   consider_constant=constants)
-
-    lr = T.scalar(name='lr')
-    f_grad_shared, f_grad_updates = eval('op.' + optimizer)(
-        lr, tparams, grads, [X], cost,
-        extra_ups=updates,
-        extra_outs=[], **optimizer_args)
-
-    t0 = time.time()
-
-    f_grad_shared(x[:batch_size])
-    f_grad_updates(0.001)
-
-    t1 = time.time()
-
-    dt = t1 - t0
-
-    print 'Time per update (%d samples per batch): %.2f' % (batch_size, dt)
-    print 'Time per epoch: %.2f' % (50000 / batch_size * dt)
-    print 'Time to best valid: %.2f' % (50000 / batch_size * dt * best_valid_epoch)
-
 def make_argument_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('experiment_dir')
@@ -213,7 +128,7 @@ def make_argument_parser():
                         help='Dataset mode: valid, test, or train')
     parser.add_argument('-s', '--samples', default=1000, type=int,
                         help='Number of posterior during eval')
-    parser.add_argument('-i', '--inference_stride', default=5, type=int)
+    parser.add_argument('-i', '--inference_steps', default=50, type=int)
     return parser
 
 if __name__ == '__main__':
@@ -246,4 +161,4 @@ if __name__ == '__main__':
     valid_scores = np.load(valid_file)
 
     eval_model(model_file, mode=args.mode, out_path=out_path, valid_scores=valid_scores,
-               posterior_samples=args.samples, inference_stride=args.inference_stride, **exp_dict)
+               posterior_samples=args.samples, steps=args.inference_steps, **exp_dict)
