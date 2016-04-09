765af2b24b9c008486872db1adfd7c8bc55e3c62
rdevon <devon@cs.unm.edu>
2016-04-09 15:55:38 -0600

    docstring for training.py

--- utils/training.py
+++ utils/training.py
@@ -1,5 +1,8 @@
 '''
 Generic training scripts.
+
+These scripts are meant as a basic example for building scripts for training,
+not as a basis for all training.
 '''
 
 import argparse
@@ -33,7 +36,15 @@ from tools import (
 
 
 def make_argument_parser():
-    '''Generic experiment parser'''
+    '''Generic experiment parser.
+
+    Generic parser takes the experiment yaml as the main argument, but has some
+    options for reloading, etc. This parser can be easily extended using a
+    wrapper method.
+
+    Returns:
+        parser
+    '''
     parser = argparse.ArgumentParser()
     parser.add_argument('experiment', default=None)
     parser.add_argument('-o', '--out_path', default=None,
@@ -44,8 +55,33 @@ def make_argument_parser():
     parser.add_argument('-i', '--save_images', action='store_true')
     return parser
 
+def make_argument_parser_test():
+    '''Generic experiment parser for testing.
+
+    Takes the experiment directory as the argument in command line.
+
+    Returns:
+        parser
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('experiment_dir', default=None)
+    parser.add_argument('-m', '--mode', default='valid',
+                        help='Dataset mode: valid, test, or train')
+    parser.add_argument('-b', '--best', action='store_true',
+                        help='Load best instead of last saved model.')
+    return parser
+
 def set_experiment(args):
-    '''Generic experiment setup method'''
+    '''Generic experiment setup method.
+
+    Extracts args from a parser like from `make_argument_parser`. These are
+    extracted into a dictionary for kwargs.
+
+    Args:
+        args: argparse args.
+    Returns:
+        exp_dict: dict, dictionary of experiment arguments.
+    '''
     args = vars(args)
     load_model = args.pop('load_model')
     load_last = args.pop('load_last')
@@ -78,21 +114,97 @@ def set_experiment(args):
     exp_dict['model_to_load'] = model_to_load
     return exp_dict
 
+def reload_model(args):
+    '''Reloads a model from argparse args.
+
+    Extracts model into a dictionary from argparse args like from
+    `make_argument_parser_test`.
+
+    Args:
+        args: argparse args.
+    Returns:
+        exp_dict: dict, dictionary of experiment arguments.
+    '''
+    exp_dir = path.abspath(args.experiment_dir)
+    out_path = path.join(exp_dir, 'results')
+    if not path.isdir(out_path):
+        os.mkdir(out_path)
+
+    try:
+        yaml = glob(path.join(exp_dir, '*.yaml'))[0]
+        print 'Found yaml %s' % yaml
+    except:
+        raise ValueError('yaml file not found. Cannot reload experiment.')
+
+    exp_dict = load_experiment(path.abspath(yaml))
+    exp_dict['out_path'] = out_path
+
+    try:
+        if args.best:
+            tag = 'best'
+        else:
+            tag = 'last'
+        model_file = glob(path.join(exp_dir, '*%s*npz' % tag))[0]
+        print 'Found %s in %s' % (tag, model_file)
+    except:
+        raise ValueError()
+
+    exp_dict['model_to_load'] = model_file
+    args = vars(args)
+    args = OrderedDict((k, v) for k, v in args.iteritems() if v is not None)
+    exp_dict.update(**args)
+    return exp_dict
+
 def set_model(create_model, model_to_load, unpack):
-    '''Generic method for creating new or loading old model'''
+    '''Convenience method for creating new or loading old model.
+
+    Object creation often can be reduced to 3 things: a creation method,
+    a saved model, and a means of unpacking the saved model. This method
+    attempts to summarize these into one method.
+
+    Args:
+        create_model: python functional, method for creating model.
+            No arguments. Methods should be defined out-of-method, e.g.:
+            `
+            dim = 100
+            def create_model():
+                return ModelClass(dim)
+            `
+            and then passed into `set_model`
+        model_to_load: str, path the npz file.
+        unpack, python function name, Takes model_to_load.
+            See `utils.tools.load_model` for details.
+    Returns:
+        models: Dict of Layer subclass objects
+    '''
     if model_to_load is not None:
         models, _ = load_model(model_to_load, unpack)
     else:
         models = create_model()
     return models
 
-def set_params(tparams, updates):
-    '''Sets params, removing updates from tparams'''
+def set_params(tparams, updates, excludes=[]):
+    '''Sets params, removing updates from tparams.
+
+    Convenience function to extract the theano parameters that will have
+    gradients calculated.
+
+    Arg:
+        tparams: OrderedDict of Theano shared variables.
+        updates: OrderedUpdates. Used to exclude variables that have gradients
+            calculated.
+        excludes: list of str. List of keys to exclude from gradients or
+            learning.
+    Returns:
+        tparams: OrderedDict of Theano shared variables that will have gradients
+            calculated.
+        all_params: OrderedDict of Theano shared variables that will be saved.
+    '''
     all_params = OrderedDict((k, v) for k, v in tparams.iteritems())
 
     tparams = OrderedDict((k, v)
         for k, v in tparams.iteritems()
-        if (v not in updates.keys()))
+        if (v not in updates.keys()) and (k not in excludes))
 
     print 'Learned model params: %s' % tparams.keys()
     print 'Saved params: %s' % all_params.keys()
@@ -113,11 +225,23 @@ def set_optimizer(inputs, cost, tparams, constants, updates, extra_outs,
     return f_grad_shared, f_grad_updates, learning_args
 
 def test(data_iter, f_test, f_test_keys, n_samples=None):
-    '''Tests the model using a data iterator'''
+    '''Tests the model using a data iterator.
+
+    Args:
+        data_iter: Dataset object.
+        f_test: Theano funciton.
+        f_test_keys: list of str. The keys that go with the corresponding list
+            of outputs from `f_test`.
+        n_samples: int (optional). If not None, use only this number of samples
+            as input to `f_test`.
+    Returns:
+        results: OrderedDict of np.array.
+    '''
     data_iter.reset()
     maxvalid = data_iter.n
 
-    widgets = ['Testing (%s set): ' % data_iter.mode, Percentage(), ' (', Timer(), ')']
+    widgets = ['Testing (%s set): ' % data_iter.mode, Percentage(),
+               ' (', Timer(), ')']
     pbar    = ProgressBar(widgets=widgets, maxval=maxvalid).start()
     results = OrderedDict()
     while True:
@@ -148,7 +272,23 @@ def test(data_iter, f_test, f_test_keys, n_samples=None):
 
 def validate(tparams, results, best_valid, e, best_epoch,
              save=None, valid_key=None, valid_sign=None, bestfile=None):
-    '''Generic validation method'''
+    '''Generic validation method.
+
+    Compares the validation result against previous best.
+
+    Args:
+        tparams: OrderedDict of Theano shared variables. For saving params.
+        results: OrderedDict of np.array.
+        best_valid: float. Best pervious value.
+        e: int. Epoch
+        best_epoch: int. Epoch for best_valid.
+        save: function. Method for saving params.
+        valid_key: str. Key from results to test against best_valid.
+        bestfile: str. Path to best file.
+    Returns:
+        best_valid: float
+        best_epoch: int
+    '''
     valid_value = results[valid_key]
     if valid_sign == '-':
         valid_value *= -1
@@ -180,7 +320,35 @@ def main_loop(train, valid, tparams,
               extra_outs_keys=None,
               output_every=None,
               **validation_args):
-    '''Generic main loop'''
+    '''Generic main loop.
+
+    Typical main loop. For special applications, a different loop is better.
+
+    Args:
+        train: Dataset object. Training dataset.
+        valid: Dataset object. Validation dataset.
+        tparams: OrderedDict of Theano.shared. Parameters of the model.
+        f_grad_shared: Theano function. Computes gradients.
+        f_grad_updates: Theano function. Updates parameters.
+        f_test: Theano function. Tests model are returns results.
+        f_test_keys: list of str. List of keys that go with `f_test`.
+        f_extra: Theano or python function. Function that is run just prior to
+            testing.
+        test_every: int (optional). If not None, then controls how many epochs
+            before test.
+        name: str. Name of experiment.
+        save: function. Saving function for parameters.
+        save_images: function (optional). Function to save images.
+        epochs: int. Number of training epochs.
+        learning_rate: float.
+        learning_rate_schedule: OrderedDict. For scheduling learning rate.
+        monitor: utils.monitor.Monitor.
+        out_path: str. Director path for output files.
+        extra_outs_keys: list of str. Keys for extra outs of `f_grad_shared`.
+        output_every: int (optional). If not None, print rvals from
+            `f_grad_shared`.
+        validation_args: kwargs. Arguments for test.
+    '''
 
     best_valid = float('inf')
     best_epoch = 0
@@ -255,7 +423,7 @@ def main_loop(train, valid, tparams,
 
             rval = f_grad_shared(x)
             if output_every is not None and s % output_every == 0:
-                #print rval
+                print rval
                 if save_images is not None:
                     save_images()
             check_bad_nums(rval, extra_outs_keys)
@@ -266,16 +434,19 @@ def main_loop(train, valid, tparams,
             s += 1
 
     except KeyboardInterrupt:
-        print 'Training interrupted'
+        print 'Training interrupted.'
 
-    if out_path is not None:
-        outfile = path.join(out_path, '{name}_{t}.npz'.format(name=name, t=int(time.time())))
-        last_outfile = path.join(out_path, '{name}_last.npz'.format(name=name))
+    try:
+        if out_path is not None:
+            outfile = path.join(out_path, '{name}_{t}.npz'.format(name=name, t=int(time.time())))
+            last_outfile = path.join(out_path, '{name}_last.npz'.format(name=name))
 
-        if save is not None:
-            print 'Saving'
-            save(tparams, outfile)
-            save(tparams, last_outfile)
-            print 'Done saving.'
+            if save is not None:
+                print 'Saving'
+                save(tparams, outfile)
+                save(tparams, last_outfile)
+                print 'Done saving.'
+    except KeyboardInterrupt:
+        print 'Saving interupted.'
 
-    print 'Bye bye!'
+    print 'Main loop finished.'
